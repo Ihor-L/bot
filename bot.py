e81969be1194e6efae68d674b812b316539901a9
Ihor-L <ihor.liulko@gmail.com>
2017-09-15 10:33:53 +0300

    '15_09_2017'

--- bot.py
+++ bot.py
@@ -1,3 +1,16 @@
 import requests
+import datetime
 
-requests.get('https://api.telegram.org/bot383163486:AAF5llYAvHZqCkdF4H6FQegwhJQCbUFfjic/sendMessage?chat_id=385220023&text=Ok')
+def main():
+    now = datetime.datetime.now()
+    hour = now.hour
+    while True:
+        now = datetime.datetime.now()
+        current_hour = now.hour
+        if 3 < current_hour < 19 and hour < current_hour:
+            requests.get('https://api.telegram.org/bot383163486:AAF5llYAvHZqCkdF4H6FQegwhJQCbUFfjic/sendMessage?chat_id=385220023&text=Ok')
+            hour = current_hour
+
+
+if __name__=='__main__':
+    main()
